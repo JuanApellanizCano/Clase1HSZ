7a5dc468a51a5e002f663849f04ac7375f9ea3a7
Alberto Saz <saz.sialb22@zaragoza.salesianos.edu>
2023-02-08 13:45:36 +0100

    Primera version con Proyecto

---
+++ Clase1HSZ/src/Clase1HSZ/serie.java
@@ -0,0 +1,20 @@
+package Clase1HSZ;
+
+import java.util.Scanner;
+
+public class serie {
+    public static void main(String[] args) {
+        serieSuma();
+    }
+    public static void serieSuma() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Número: ");
+        int numero = sc.nextInt();
+        sc.close();
+        int suma = 0;
+        for (int i = 1; i <= numero; i++) {
+            suma += i;
+        }
+        System.out.printf("Suma de los %d primeros números = %d", numero, suma);
+    }
+}
